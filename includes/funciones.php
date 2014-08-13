d039a5f2ed350b7debb16ec1decaada3b925f218
beehoney <ale@yummyclub.com.ve>
2014-08-12 23:44:03 -0430

    Agregamos los archivos del ftp
    IMPORTANTE: NUNCA DESCARGAR LOCALMENTE LOS ARCHIVOS DEL FTP QUE CONECTAN
    CON LA BASE DE DATOS

---
+++ includes/funciones.php
@@ -0,0 +1,123 @@
+<?php
+ 
+ 
+include( '../wp-load.php');
+/**echo 'Honey'; 
+
+            $user_name = 'Carlos'; //$_POST['user'];
+            $email_address = 'soto@gmail.com'; //$_POST['email'];
+            $password = 'Carlos123'; //$_POST['pass'];
+            $resultado = array();
+            **/
+
+function Hola(){
+    return 'Hola Darwin'; 
+}
+
+function consultapost($losids,$ndelciclo){
+query_posts( array( 'category__and' => $losids ) );
+$contador=0;
+while (have_posts()) : the_post();
+    $array[$contador]['titulo'] =  get_the_title();
+    $array[$contador]['texto'] =  get_the_content();
+    $array[$contador]['imagen'] =  urlimagen($post->ID);
+    $array[$contador]['categorias'] =  lascats($post->ID);
+    $contador++;
+endwhile;
+return $array; 
+}
+
+function urlimagen($postid) {
+    return  wp_get_attachment_url( get_post_thumbnail_id($postid) );
+}
+
+function lascats($postid) {
+    $cats = "";
+foreach((get_the_category($postid)) as $category) {
+      $cats .= $category->cat_ID.' ';
+}
+    return  $cats;
+}
+
+
+
+function Ingresar($user,$pass){
+         $user = wp_authenticate($user,$pass);
+ // User Loop
+        if ( !is_wp_error($user)  ) {
+                 $array['usuario']['id'] =  $user->ID;
+                $array['usuario']['nombre'] =  $user->display_name;
+                $array['usuario']['usuario'] =  $user->user_login;
+                $array['usuario']['correo'] =  $user->user_email;   
+                $array['status'] = true;       
+        } else {
+            $array['status'] = false;  
+              $array["error_msg"] = "Usuario o password incorrecto";
+            
+        }
+        return $array;
+    }
+
+function IngresarWP($user,$pass) {
+        $array_log = array();
+        
+        $creds = array();
+        $creds['user_login'] = $user;
+        $creds['user_password'] = $pass;
+        $creds['remember'] = false;
+        $user = wp_signon( $creds, false );
+        if ( is_wp_error($user) )
+            $user = $user->get_error_message();
+        return $user;
+    }
+
+
+
+
+
+
+    function Registrar($user,$pass,$email){ 
+        $array = array();
+            if( null == username_exists( $user) ) {
+            
+                // Generate the password and create the user
+                // $password = wp_generate_password( 12, false );
+                $user_id = wp_create_user( $user, $pass, $email );
+            
+                // Set the nickname
+                wp_update_user(
+                array(
+                'ID'          =>    $user_id,
+                'display_name' => $user
+                
+                )
+                );
+            
+
+                // Set the role
+                $user = new WP_User( $user_id );
+                $user->set_role( 'subscriber' );
+            
+                // Email the user
+                $data = wp_mail( $email_address, 'Bienvenido a YummyClub!', 'Aqui tienes tu contraseña: ' . $password . ', Esperamos que no se te olvide ;)');
+            $resultado = array('Aviso:'=>'Registro Existoso!', 'status:' => true);
+            
+                 $array['usuario']['id'] =  $user->ID;
+                $array['usuario']['nombre'] =  $user->display_name;
+                $array['usuario']['usuario'] =  $user->user_login;
+                $array['usuario']['correo'] =  $user->user_email;   
+                $array['status'] = true;  
+
+
+            
+            } else{
+                $array =  array('Aviso:'=>'El correo esta registrado', 'status:' => false);
+            }
+
+            return $array;
+
+}
+
+
+ 
+?>
---
+++ index.php
@@ -0,0 +1,104 @@
+<?php
+// METODO POR HONEY
+//echo dirname(__FILE__);
+include('includes/funciones.php');
+/* 
+
+ //TRAMPA
+        Entrar
+    $_POST['etiq'] = 'login';
+    $_POST['pass'] = '123456';
+    $_POST['user'] = 'rafale';
+        
+        Registrar
+
+    $_POST['etiq'] = 'register';
+    $_POST['user'] = 'user5';
+    $_POST['pass'] = '123456';
+    $_POST['email'] = 'user@asa.com';
+
+*/
+if (isset($_POST['etiq']) && $_POST['etiq'] != '') {
+    // get etiq
+    $etiq = $_POST['etiq'];
+
+ 
+    // check for tag type
+    if ($etiq == 'login') {
+        // Request type is check Login
+       // $email = $_POST['email'];
+        $pass = $_POST['pass'];
+ 		$user = $_POST['user'];
+
+
+
+        // check for user
+
+        $respuesta = Ingresar($user,$pass);
+            //echo 'Hola ' . $user . '-' . $pass;
+        if ($respuesta['status']) {
+            // user found
+            //array_pop($respuesta);
+              echo json_encode($respuesta);
+        } else {
+            // user not found
+            // echo json with error = 1
+          
+            echo json_encode($respuesta);
+        }
+    } else if ($etiq == 'register') {
+        // Request type is Register new user
+        $user = $_POST['user'];
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
+            
+        $respuesta = Registrar($user,$pass,$email);
+       echo json_encode($respuesta);
+    } else {
+        echo "Solicitud Invalida";
+    }
+} else {
+    echo "Acceso Denegado";
+}
+
+/* TRAMPA PARA SOLICITAR DATOS DE POST */
+echo "<br><br>"; //ESTO ES PARA BAJAR LA LINEA
+$losids=array(11,11); // ESTOS SON LOS ID DE LAS CATEGORIAS QUE SE QUIEREN CONSULTAR
+/*
+DIRECTORIO RAPIDO DE IDS
+Sitios=28
+—Atributos=10
+— — Aire libre=15
+— — Alcohol=16
+— — Delivery=19
+— — Estacionamiento=17
+— — Reservas=18
+— — Wi-Fi=29
+
+— Ciudad=7
+— — Maracaibo=11
+
+— Nivel=9
+— — Basico=12
+— — Gold=14
+— — Platinum=13
+
+— Tipo=8
+— — Arabe=20
+— — Carnes=21
+— — China=22
+— — Hamburguesas=27
+— — Italiana=23
+— — Japonesa=24
+— — Pizzerías=25
+— — Postres y Dulces=26
+— — Saludable=30
+
+*/
+$ndelciclo=99;  // ESTE ES EL LIMITE DE LA PETICION
+$datosposts=consultapost($losids,$ndelciclo); // AQUI LLAMA A LA FUNCION
+echo json_encode($datosposts); //Y ACA LA IMPRIME EN JSON
+/* FIN DE LA TRAMPA DATOS POST */
+
+
+?>
